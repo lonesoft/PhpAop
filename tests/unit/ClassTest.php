1f546a3123e4957046880284b8db8e2bcecc22bc
Lorenzo <lorenzomengon@gmail.com>
2022-03-28 16:57:43 +0100

    Split the tests with no AOP in its own file

---
+++ tests/unit/ClassTest.php
@@ -0,0 +1,79 @@
+<?php
+
+use Lonesoft\PhpAop\Aop;
+use Lonesoft\PhpAop\JointPoint;
+
+class BaseTest extends AbstractTest
+{
+
+
+    // PUBLIC METHODS
+
+    public function testCallPublicMethod()
+    {
+        $actual = $this->class->testCallPublic();
+
+        $this->assertEquals('callPublic', $actual);
+    }
+
+
+    // PROTECTED METHODS
+
+    public function testCallProtectedMethod()
+    {
+        $actual = $this->class->testCallProtected();
+
+        $this->assertEquals('callProtected', $actual);
+    }
+
+
+    // PRIVATE METHODS
+
+    public function testCallPrivateMethod()
+    {
+        $actual = $this->class->testCallPrivate();
+
+        $this->assertEquals('callPrivate', $actual);
+    }
+
+
+    // STATIC METHODS
+
+    public function testCallStaticMethod()
+    {
+        $actual = $this->class->testCallStatic();
+
+        $this->assertEquals('callStatic', $actual);
+    }
+
+
+    // METHODS WITH PARAMETERS
+
+    public function testCallMethodWithParameters()
+    {
+        $actual = $this->class->testCallWithParameters(1, 'a', false, 'not-null');
+
+        $this->assertEquals('callWithParameters(1, a, , not-null)', $actual);
+    }
+
+
+    // METHODS WITH DEFAULT PARAMETERS
+
+    public function testCallMethodWithDefaultParameters()
+    {
+        $actual = $this->class->testCallWithDefaultParameters();
+
+        $this->assertEquals('callWithParameters(1, default, 1, )', $actual);
+    }
+
+
+    // METHODS WITH TYPED PARAMETERS
+
+    public function testCallMethodWithTypedParameters()
+    {
+        $actual = $this->class->testCallWithTypedParameters(new stdClass());
+
+        $this->assertEquals('callWithTypedParameters', $actual);
+    }
+
+}
